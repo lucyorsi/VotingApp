b6239d85fdc8cb361e8d9ee92d769305a6422423
jackyzq <jackyzqting@hotmail.com>
2016-04-30 18:41:05 +0800

    worked on setup

---
+++ js/setup.js
@@ -0,0 +1,6 @@
+document.getElementById("uploadBtn").onchange = function () {
+    document.getElementById("uploadFile").value = this.value;
+};
+document.getElementById("uploadBtn2").onchange = function () {
+    document.getElementById("uploadFile2").value = this.value;
+};

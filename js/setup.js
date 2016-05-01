da17d048dc4c9a7fc76e73dc1bbc5d554d1cd22f
ysong4 <sy568386042@gmail.com>
2016-05-01 13:53:58 +0800

    setup.html

--- js/setup.js
+++ js/setup.js
@@ -4,3 +4,11 @@ document.getElementById("uploadBtn").onchange = function () {
 document.getElementById("uploadBtn2").onchange = function () {
     document.getElementById("uploadFile2").value = this.value;
 };
+
+$('#min-date').bootstrapMaterialDatePicker({ format : 'DD/MM/YYYY HH:mm', minDate : new Date() });
+
+//auto expand textarea
+function adjust_textarea(h) {
+    h.style.height = "20px";
+    h.style.height = (h.scrollHeight)+"px";
+}
